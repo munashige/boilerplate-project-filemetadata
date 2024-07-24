35dd2d5f2b4a24152a1f0f21eb820bb9da108733
StanislavN <s.nikiforov@conundrum.ai>
2024-07-24 23:49:18 +0300

    Import solution

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", multer().single("upfile"), (request, response) => {
+  let responseObject = {};
+  responseObject["name"] = request.file.originalname;
+  responseObject["type"] = request.file.mimetype;
+  responseObject["size"] = request.file.size;
 
-app.post('/api/fileanalyse',(req,res) => {
-  
+  response.json(responseObject);
 });
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
